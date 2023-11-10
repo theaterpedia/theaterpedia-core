ddb49f6a050aa39cbb330d6539d75215d8aeb796
Hans <hans.doenitz@dasei.eu>
2023-11-10 15:25:39 +0100

    added apps/tp-template

---
+++ apps/tp-template/app.config.ts
@@ -0,0 +1,5 @@
+export default defineAppConfig({
+  theme: {
+    name: "Hello from Papillon"
+  }
+})
---
+++ apps/tp-template/app.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+    <!-- <NuxtWelcome /> -->
+    <HelloWorld />
+  </div>
+</template>
