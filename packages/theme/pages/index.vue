6d660a1aa3f8335ca6013a14b221bd3a5133d8dd
Hans <hans.doenitz@dasei.eu>
2023-12-16 08:09:54 +0100

    Remove html-validation and update homepage content

--- packages/theme/pages/index.vue
+++ packages/theme/pages/index.vue
@@ -1,12 +1,9 @@
 <template>
-  <div v-for="(component, index) in content" :key="`${component.fields.component}-${index}`">
-    <RenderContent :content="component.fields.content" />
+  <div>
+    <h1>Vercel-Deployment is live</h1>
   </div>
 </template>
 
 <script setup lang="ts">
-import { useSfContent } from '../composables/useSfContent';
-const { getContent, data: content } = useSfContent('home-page');
 
-getContent();
 </script>
