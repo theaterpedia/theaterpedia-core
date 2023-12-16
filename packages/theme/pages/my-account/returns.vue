b602e07d7a26de2608a8d6165291ee13a74780db
Hans <hans.doenitz@dasei.eu>
2023-12-16 08:29:53 +0100

    killed image-partials for order/success and my-account/returns

--- packages/theme/pages/my-account/returns.vue
+++ packages/theme/pages/my-account/returns.vue
@@ -4,13 +4,6 @@
     {{ $t('account.ordersAndReturns.returnsHeading') }}
   </h2>
   <div v-if="!data?.length" class="col-span-3 text-center">
-    <NuxtImg
-      src="/images/returns.png"
-      :alt="$t('account.ordersAndReturns.returnsAltText')"
-      width="192"
-      height="192"
-      class="mx-auto"
-    />
     <h3 class="typography-headline-3 font-bold mt-6 mb-4">
       {{ $t('account.ordersAndReturns.noOrders') }}
     </h3>
