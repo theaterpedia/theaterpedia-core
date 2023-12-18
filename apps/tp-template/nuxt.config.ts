da2c179db5b5aafea49f04c7782a63fb2fc5660e
Hans <hans.doenitz@dasei.eu>
2023-12-18 11:42:03 +0100

    ssr almost undone

--- apps/tp-template/nuxt.config.ts
+++ apps/tp-template/nuxt.config.ts
@@ -3,7 +3,30 @@ export default defineNuxtConfig({
           '@crearis/theme-main',
           '@crearis/data-main'
         ],
-  ssr: false, // #TODO _06 re-enableSSR and test on github-pages
+    nitro: {
+      prerender: {
+        routes: ['/'],
+        ignore: [
+        '/category',
+        '/product/',
+        '/cart',
+         '/checkout',
+        '/search',
+        '/my-account',
+        '/order/success',
+        '/order/failed',
+        '/my-account/personal-data',
+        '/my-account/billing-details',
+        '/my-account/shipping-details',
+        '/my-account/my-orders',
+        '/my-account/returns',
+        '/reset-password',
+        '/reset-password-success',
+        '/set-new-password',
+        '/login',
+        '/signup']
+      }
+    }, // #TODO _05 enable-full-nitro-prerender
   // #TODO _05 html-validation modules: ['nuxt-hydration', '@nuxtjs/html-validator'],
   devtools: { enabled: true },
 })
