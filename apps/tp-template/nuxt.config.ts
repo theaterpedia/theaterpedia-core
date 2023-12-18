b810ecc4c59fd4ca720cf3c986a2426e81268bb1
Hans <hans.doenitz@dasei.eu>
2023-12-18 11:33:06 +0100

    turn on ssr

--- apps/tp-template/nuxt.config.ts
+++ apps/tp-template/nuxt.config.ts
@@ -3,7 +3,7 @@ export default defineNuxtConfig({
           '@crearis/theme-main',
           '@crearis/data-main'
         ],
-  ssr: false,
+  ssr: false, // #TODO _06 re-enableSSR and test on github-pages
   // #TODO _05 html-validation modules: ['nuxt-hydration', '@nuxtjs/html-validator'],
   devtools: { enabled: true },
 })
