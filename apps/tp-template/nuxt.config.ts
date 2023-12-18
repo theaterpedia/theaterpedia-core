225130868132212199db77ff9934fd5845c956c9
Hans <hans.doenitz@dasei.eu>
2023-12-18 11:31:34 +0100

    turn off ssr

--- apps/tp-template/nuxt.config.ts
+++ apps/tp-template/nuxt.config.ts
@@ -3,6 +3,7 @@ export default defineNuxtConfig({
           '@crearis/theme-main',
           '@crearis/data-main'
         ],
+  ssr: false,
   // #TODO _05 html-validation modules: ['nuxt-hydration', '@nuxtjs/html-validator'],
   devtools: { enabled: true },
 })
