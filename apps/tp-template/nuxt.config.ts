3ae4154a25dd8eebf0ed43e5082e98682443c8b8
Hans <hans.doenitz@dasei.eu>
2023-12-18 12:02:00 +0100

    partly ssr

--- apps/tp-template/nuxt.config.ts
+++ apps/tp-template/nuxt.config.ts
@@ -10,7 +10,23 @@ export default defineNuxtConfig({
     prerender: {
       routes: ['/'],
       ignore: [
-      '/product/',]
+      '/product/',
+      '/cart',
+      '/checkout',
+      '/search',
+      '/my-account',
+      '/order/success',
+      '/order/failed',
+      '/my-account/personal-data',
+      '/my-account/billing-details',
+      '/my-account/shipping-details',
+      '/my-account/my-orders',
+      '/my-account/returns',
+      '/reset-password',
+      '/reset-password-success',
+      '/set-new-password',
+      '/login',
+      '/signup']
     }
     }, // #TODO _05 enable-full-nitro-prerender
   // #TODO _05 html-validation modules: ['nuxt-hydration', '@nuxtjs/html-validator'],
