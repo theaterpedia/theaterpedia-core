7f459f58514a86b56419a6429ab5c2a693113065
Hans <hans.doenitz@dasei.eu>
2024-01-09 11:51:52 +0100

    Add OdooModule to sdk.config.ts

--- apps/tp-template/sdk.config.ts
+++ apps/tp-template/sdk.config.ts
@@ -1,5 +1,7 @@
 import { SdkModule, sdkModule } from '@crearis/crearis-boilerplate-sdk';
+import { OdooModule, OdooModuleType } from '@erpgap/odoo-sdk';
 
-export default defineSdkConfig(({ buildModule }) => ({
-  commerce: buildModule<SdkModule>(sdkModule),
+export default defineSdkConfig(({ buildModule, middlewareUrl }) => ({
+  commerce: buildModule(sdkModule), 
+  odoo: buildModule(OdooModule, {apiUrl: middlewareUrl + 'api/odoo/', ofetch: useFetch}),
 }));
--- packages/data/sdk.config.ts
+++
@@ -1,5 +0,0 @@
-import { SdkModule, sdkModule } from '@crearis/crearis-boilerplate-sdk';
-
-export default defineSdkConfig(({ buildModule }) => ({
-  commerce: buildModule<SdkModule>(sdkModule),
-}));
