fd7993b58b95a15e4ea6410d16a62952b72d9750
Hans <hans.doenitz@dasei.eu>
2024-01-24 07:38:15 +0100

    add index.vue

---
+++ packages/theme/pages/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <NuxtLayout
+    name="dev"
+    back-href="paths.home"
+    back-label-desktop="Homepage"
+    back-label-mobile="Home"
+    heading="Default-Shop-Page"
+  >
+  <div class="prose">
+    <h1 class="text-4xl font-mona">DEV && Admin-Page</h1>
+    <p class="text-xl font-mona">
+      here you find dev-content for testing-purpose and for managing the migration from the vsf-shop to the pruvious-driven setup
+    </p>
+    <p class="text-xl font-mona">
+      premium
+    </p>
+    <p class="text-xl font-mono">
+      premium
+    </p>
+    <nuxt-link to="shop">VSF-Shop-Page</nuxt-link>
+  </div>
+  </NuxtLayout>
+</template>
+
+<script setup lang="ts">
+</script>
