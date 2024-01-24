94df8d5c03d9fa413a9400238962273c9f9554e8
Hans <hans.doenitz@dasei.eu>
2024-01-24 06:46:16 +0100

    disable prerendering

--- packages/data/nuxt.config.ts
+++ packages/data/nuxt.config.ts
@@ -14,7 +14,7 @@ export default defineNuxtConfig({
   ],
   nitro: {
     prerender: {
-      crawlLinks: true,
+      crawlLinks: false,
       ignore: ['/shop'],
     },
     compressPublicAssets: true,
