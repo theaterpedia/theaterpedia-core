67ae93196dcd5276048c0013afa7a0cb5d961626
Hans <hans.doenitz@dasei.eu>
2024-01-24 07:03:28 +0100

    nitro.prerender disable failOnError

--- packages/data/nuxt.config.ts
+++ packages/data/nuxt.config.ts
@@ -15,6 +15,7 @@ export default defineNuxtConfig({
   nitro: {
     prerender: {
       crawlLinks: false,
+      failOnError: false,
       ignore: ['/shop'],
     },
     compressPublicAssets: true,
