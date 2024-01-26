b8bf30155b596f7fff12357753ee1ba1e2459fa3
Hans <hans.doenitz@dasei.eu>
2024-01-26 15:09:00 +0100

    Monaspace-Logo

--- packages/theme/components/ui/TpLogo.vue
+++ packages/theme/components/ui/TpLogo.vue
@@ -1,6 +1,6 @@
 <template>
   <div :class="logoClass" class="pl-2 md:pl-4 lg:pl-6 xl:pl-8 font-semibold">
-    <h1 :class="props.logoSize === 'default' ? 'text-2xl sm:text-4xl lg:text-5xl xl:text-6xl 2xl:text-7xl' : props.logoSize === 'sm' ? 'text-xl sm:text-2xl md:text-3xl lg:text-4xl xl:text-5xl' : 'text-4xl sm:text-5xl lg:text-7xl 2xl:text-8xl'">
+    <h1 :class="props.logoSize === 'default' ? 'font-mona text-xl sm:text-2xl lg:text-3xl xl:text-4xl 2xl:text-5xl' : props.logoSize === 'sm' ? 'font-mona text-md sm:text-lg md:text-xl lg:text-2xl xl:text-3xl' : 'font-mona text-3xl sm:text-4xl lg:text-5xl 2xl:text-6xl'">
       Theater<span class="text-primary">pedia</span>
     </h1>
     <h2 v-show="extended" :class="props.logoSize === 'default' ? 'invisible sm:visible text-sm tracking-tight lg:pl-1 lg:text-lg xl:pl-none xl:tracking-tighter xl:text-2xl 2xl:pl-3 2xl:tracking-wide 2xl:text-2xl' : props.logoSize === 'sm' ? 'invisible lg:visible text-sm lg:tracking-tight xl:text-lg xl:pl-1.5' : 'tracking-wider text-xs sm:text-base lg:text-2xl 2xl:text-4xl 2xl:tracking-tight'">
