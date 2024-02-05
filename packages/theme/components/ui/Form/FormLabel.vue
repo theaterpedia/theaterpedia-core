0a8209f4faebbebdf3b65537bd24b1545d105954
Hans <hans.doenitz@dasei.eu>
2024-02-05 12:12:39 +0100

    removed all unsused components, pages, utils from theme-layer > theme-dev or data-dev

--- packages/theme/components/ui/Form/FormLabel.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <span class="block text-sm font-medium pb-0.5" data-testid="form-label">
-    <slot />
-  </span>
-</template>
--- packages/theme/components/ui/ProductCardHorizontal/types.ts
+++
@@ -1,8 +0,0 @@
-import type { SfProduct } from '@vue-storefront/unified-data-model';
-
-export type ProductHorizontalProps = {
-  product: Omit<
-    SfProduct,
-    'id' | 'sku' | 'slug' | 'description' | 'price' | 'primaryImage' | 'rating' | 'variants' | 'quantityLimit'
-  >;
-};
