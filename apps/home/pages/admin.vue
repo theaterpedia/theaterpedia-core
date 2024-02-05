3cc068978b633ab3abc4f66de18dc3bc8cc9d663
Hans <hans.doenitz@dasei.eu>
2024-02-05 12:53:48 +0100

    added stub-pages category/id + admin to apps/home

--- apps/home/pages/admin.vue
+++ apps/home/pages/admin.vue
@@ -6,10 +6,15 @@
 </script>
 
 <template>
-  <h1>Index Old</h1>
   <!-- #TODO _7 ReEnable SfContent-Logic></! -->
   <!-- div v-for="(component, index) in content" :key="`${component.fields.component}-${index}`">
     <RenderContent :content="component.fields.content" />
   </! -->
+  <NuxtLayout>
+    <div class="prose py-8 px-8">
+      <h1>Index Old</h1>
+      <NuxtLink to="/">Home</NuxtLink>
+    </div>
+  </NuxtLayout>
   <!-- ContentDoc / -->
 </template>
