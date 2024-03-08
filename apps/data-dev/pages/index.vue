46034d8d420b994f5c4f9d24762744719d3261e3
Hans <hans.doenitz@dasei.eu>
2024-03-08 15:57:11 +0100

    add breadcrumbs and default-layout

--- apps/data-dev/pages/index.vue
+++ apps/data-dev/pages/index.vue
@@ -1,18 +1,21 @@
 <script setup lang="ts">
 const { loadCategoryList, categories } = useCategory();
+const breadcrumbs = ref([{ name: 'Home', link: '/' }]);
 
 await loadCategoryList({ filter: { parent: true } });
 </script>
 
 <template>
-  <MainBanner />
-  <CategoryCard :categories="categories" />
-  <NuxtLazyHydrate when-visible>
-    <LazyDisplay />
-  </NuxtLazyHydrate>
-  <section class="pb-16">
+  <NuxtLayout name="default" :breadcrumbs="breadcrumbs">
+    <MainBanner />
+    <CategoryCard :categories="categories" />
     <NuxtLazyHydrate when-visible>
-      <LazyProductSlider heading="Inspired by your picks" />
+      <LazyDisplay />
     </NuxtLazyHydrate>
-  </section>
+    <section class="pb-16">
+      <NuxtLazyHydrate when-visible>
+        <LazyProductSlider heading="Inspired by your picks" />
+      </NuxtLazyHydrate>
+    </section>
+  </NuxtLayout>
 </template>
