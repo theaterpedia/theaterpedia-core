3240983ce3ba33d76a8b81039d8c2750a6cd6a91
Muris Ceman <murisceman@gmail.com>
2024-04-16 14:09:34 +0200

    chore: add types

--- packages/data/server/middleware/04-auth.ts
+++ packages/data/server/middleware/04-auth.ts
@@ -2,18 +2,19 @@
 import { query } from '#pruvious/server'
 import { Endpoints } from '@erpgap/odoo-sdk-api-client'
 import { defineEventHandler, getCookie } from 'h3'
+import { Partner } from '../../graphql'
 
 export default defineEventHandler(async (event) => {
   const api: Endpoints = event.context.apolloClient.api
   const cookie = getCookie(event, 'session_id')
  
   if (cookie) {
-    const response = await api.query({ queryName: 'LoadUserQuery' } as any, null)
-    
-    if ((response.data as any)?.partner) {
+    const response = await api.query<any, { partner: Partner }>({ queryName: 'LoadUserQuery' } as any, null)
+   
+    if (response.data?.partner) {
       const user = await query('users')
         .deselect({ password: true })
-        .where('email', (response.data as any)?.partner.email)
+        .where('email', response.data.partner.email)
         .populate()
         .first()
       
